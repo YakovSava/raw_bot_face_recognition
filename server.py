621baa67e36f82bbd5a922235370a51a0d515884
Jacob Savel'ev <g6h6m238929@gmail.com>
2022-12-30 16:02:09 +0500

    Add new license: GNU GPL v2

--- server.py
+++ server.py
@@ -2,13 +2,10 @@ import asyncio
 import warnings
 
 from aiohttp.web import Application, RouteTableDef, HTTPInternalServerError, run_app, Response
-from aiohttp_jinja2 import template, setup
-from jinja2 import PackageLoader
 from argparse import ArgumentParser
 from sys import platform
 from serv_plugins.storage import pages, isnull
 from serv_plugins.binder import Binder
-from serv_plugins.ip import self_ip
 
 warnings.filterwarnings('ignore')
 
@@ -22,22 +19,17 @@ app = Application()
 routes = RouteTableDef()
 binder = Binder()
 parser = ArgumentParser(description = 'Project argument')
-setup(
-	app = app,
-	loader = PackageLoader('html', 'html')
-)
 
-parser.add_argument('--host', help = 'Host to listen', default = self_ip())
+parser.add_argument('--host', help = 'Host to listen', default = 'localhost')
 parser.add_argument('--port', help = 'Port for accept connection', default = '9000')
 
-@template('main.html')
+@routes.get('/')
 async def main_page(request):
-	# some_page = await binder.get_page(pages.page.main)
-	# if isnull(some_page):
-	# 	raise HTTPInternalServerError()
-	# else:
-	# 	return Response(text = some_page, content_type='text/html')
-	return {}
+	some_page = await binder.get_page(pages.page.main)
+	if isnull(some_page):
+		raise HTTPInternalServerError()
+	else:
+		return Response(text = some_page, content_type='text/html')
 
 @routes.post(pages.link.api)
 async def api_page(request):
